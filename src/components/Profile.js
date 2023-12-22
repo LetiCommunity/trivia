418c67cc02b3c0490c346b4666070441c43baa13
heismrjava <137235277+heismrjava@users.noreply.github.com>
2023-12-22 10:10:22 +0100

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -27,7 +27,7 @@ const Profile = () => {
 
   const handleDeleteAccount = async () => {
     try {
-      await axios.delete("http://13.43.167.192/api/trivia/profiles/profile", {
+      await axios.delete("https://trivi4.com/api/trivia/profiles/profile", {
         headers,
       });
       signOut(auth)
